940c88a343e69e97d82dbf1cc6e76e41e732f440
Gautam Singh Rathore <gautamsingh25062005@gmail.com>
2024-03-26 10:49:51 +0530

    Learning Searching

---
+++ Searching/BinarySearch.java
@@ -0,0 +1,43 @@
+package JavaDSAPlayground.Searching;
+
+public class BinarySearch {
+    //BINARY SEARCH FUNCTION (**ITERATIVE APPROACH**)
+    public static int binarySearch(int arr[] , int key){
+        int start = 0 ;
+        int end = arr.length-1;
+        while(end >= start){
+            int mid = (start+end)/2;
+            if(arr[mid]==key){
+                return mid;
+            }else if(key>arr[mid]){
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        return -1;
+    }
+    // MAIN FUNCTION
+    public static void main(String[] args) {
+        // Time Complexity : O(logn)
+        //prerequisite - sorted array
+        /* Pseudo - code
+        start=0 , end=n-1
+        while(start<=end)
+            find mid
+            compare mid and key
+                mid==key FOUND
+                mid>key  LEFT
+                mid<key  RIGHT */
+        int arr[] = {2,4,6,8,10,12,14};
+        int key = 10;
+        int ans = binarySearch(arr , key);
+        if (ans == -1)
+            System.out.println(
+                    "Element is not present in array");
+        else
+            System.out.println(
+                    "Element is present at index " + ans);
+    }
+}
+
---
+++ Searching/BinarySearch2.java
@@ -0,0 +1,32 @@
+package JavaDSAPlayground.Searching;
+
+public class BinarySearch2 {
+    //BINARY SEARCH FUNCTION (**RECURSSIVE APPROACH**)
+    public static int binarySearch(int [] arr , int key , int start , int end){
+        if(start <= end){
+          int mid=(start+end)/2;
+          if(arr[mid]==key){
+              return mid;
+          }else if(key>arr[mid]){
+              return binarySearch(arr , key , mid+1 , end);
+          }else{
+              return binarySearch(arr , key , start , mid-1);
+          }
+        }
+        return -1;
+    }
+    // MAIN FUNCTION
+    public static void main(String[] args) {
+    //prerequisite - sorted array
+    int arr[] = {2,4,6,8,10,12,14};
+    int key = 100;
+    int ans = binarySearch(arr , key ,0 , arr.length -1);
+        if (ans == -1)
+            System.out.println(
+                    "Element is not present in array");
+        else
+            System.out.println(
+                    "Element is present at index " + ans);
+    }
+}
+
