564fafcec2a8f1c69531d4ea2ab08c53c119a0d5
Gautam Singh Rathore <gautamsingh25062005@gmail.com>
2024-07-07 09:21:31 +0530

    LinkedList

--- LinkedList_1/Problem4.java
+++ LinkedList_1/Problem4.java
@@ -11,5 +11,41 @@ public class Problem4 {
         ll.addFirst(3);
         ll.addFirst(2);
         ll.addFirst(1);
+        ll.print();
+        removeNthFromEnd(5);
+        ll.print();
+
+    }
+    public static int calculateLength(){
+        if(LinkedList.head == null){
+            return 0;
+        }
+        int count = 0 ;
+        LinkedList.Node temp = LinkedList.head;
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+
+    public static int  removeNthFromEnd(int n){
+        int size = calculateLength();
+        // Check if the node to be removed is the head
+        if (n == size) {
+            int data = LinkedList.head.data;
+            LinkedList.head = LinkedList.head.next;
+            return data;
+        }
+        LinkedList.Node temp = LinkedList.head;
+        for(int i=1 ; i<=size-n-1 ; i++){
+            temp = temp.next;
+        }
+         // Now temp is at the (size-n)th node, we need to remove temp.next
+        int data = temp.next.data;
+         temp.next = temp.next.next;
+        
+        return data;
+
     }
 }
