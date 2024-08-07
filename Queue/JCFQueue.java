30641f4e902ad2ce00844d7910fdf5e319f0f9c8
Gautam Singh Rathore <gautamsingh25062005@gmail.com>
2024-08-07 19:59:13 +0530

    Deque

--- Queue/JCFQueue.java
+++ Queue/JCFQueue.java
@@ -1,14 +1,14 @@
 package Queue;
 
 import java.util.ArrayDeque;
-import java.util.LinkedList;
+// import java.util.LinkedList;
 import java.util.Queue;
 
 public class JCFQueue {
 
     public static void main(String[] args) {
         
-        Queue<Integer> q1 = new LinkedList<>(); // ArrayDeque
+     // Queue<Integer> q1 = new LinkedList<>();
         Queue<Integer> q = new ArrayDeque<>();
         // Queue is not a class but an interface in Java Collection Framework
         q.add(1);
